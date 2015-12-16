cc4170435bf9d05e22859f149207ccb45ae2e500
Roman Kozlovskyi <krzroman@gmail.com>
2015-12-16 15:50:48 +0200

    Prepare release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requires = requires + [
 ]
 
 setup(name='openprocurement.chronograph',
-      version='0.4',
+      version='0.4.1',
       description='openprocurement.chronograph',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
