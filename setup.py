1f459363ba33a30b12ce9a16219c97902c86d188
Andrew Leitsius <leits@quintagroup.com>
2017-09-30 13:16:30 +0300

    Prepare openprocurement.chronograph 0.6.7+ea.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requires = requires + [
 ]
 
 setup(name='openprocurement.chronograph',
-      version='0.6.6+ea',
+      version='0.6.7+ea',
       description='openprocurement.chronograph',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
