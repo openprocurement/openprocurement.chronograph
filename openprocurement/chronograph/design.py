217bc850ee8ed8f90a26be4ab827146e771c6cca
Scandie <grizly.vl@gmail.com>
2018-10-12 15:23:54 +0300

    Add texas_streams to plan_auctions_view

--- openprocurement/chronograph/design.py
+++ openprocurement/chronograph/design.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from couchdb.design import ViewDefinition
-from openprocurement.chronograph.constants import INSIDER_WORKING_DAY_START
+from openprocurement.chronograph.constants import (
+    INSIDER_WORKING_DAY_START, TEXAS_WORKING_DAY_START
+)
 
 
 def sync_design(db):
@@ -30,6 +32,12 @@ plan_auctions_view = ViewDefinition('plan', 'auctions', '''function(doc) {
                           doc._id.split('_')[1] + 'T' + '%s');
                 }
             }
+            if (i.indexOf('texas_streams') == 0) {
+                for (var aid in doc[i]) {
+                    emit([doc[i][aid], null],
+                          doc._id.split('_')[1] + 'T' + '%s');
+                }
+            }
         };
     }
-}''' % INSIDER_WORKING_DAY_START.isoformat())
+}''' % (INSIDER_WORKING_DAY_START.isoformat(), TEXAS_WORKING_DAY_START.isoformat()))
